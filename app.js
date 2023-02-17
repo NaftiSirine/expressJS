6d3d499d60b912b93e6c99a74a1e31dadc399bc7
NaftiSirine <naftisirin30@gmail.com>
2023-02-17 14:45:16 +0100

    os Router

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const  http = require('http');
 
 // router declaration 
 var indexRouter = require('./routes/index');
-
+var osRouter= require('./routes/os');
 
 var app = express();
 
@@ -21,8 +21,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/hello', indexRouter);
-
+app.use('/', indexRouter);
+app.use('/os', osRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/os.js
+++ routes/os.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json("welcome to express");
-});
+    res.json("os router");
+  });
 module.exports = router;
