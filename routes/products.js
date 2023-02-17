5db775223fde5e6593e3e1c718b51cdc26ae00f1
NaftiSirine <naftisirin30@gmail.com>
2023-02-17 16:04:39 +0100

    product method

--- routes/products.js
+++ routes/products.js
@@ -2,9 +2,29 @@ var express = require('express');
 var router = express.Router();
 const products = require('../products.json');
 router.get('/', function (req,res,next){
-
+try {
+    if(!products ){
+        throw new Error("products not found")
+    }
     res.json(products)
+} catch (error) {
+    res.status(500).json(error.message)
+}
+   
 })
 
+router.get('/:id',(req,res,next)=>
+{
+    const {id} = req.params;
+    if(!products ){
+        throw new Error("products not found")
+    }
+    if(!products[id.toUpperCase()]) {
+        throw new Error("id must be valid")
+
+    }
+   
+    res.json(products[id.toUpperCase()])
+})
 
 module.exports = router;
